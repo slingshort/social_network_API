b08a4534b32ee1569783fdc83b593cf22f58bba9
slingshort <salina.myat@gmail.com>
2022-12-11 13:41:01 +1100

    updated reaction model

--- main/models/Reaction.js
+++ main/models/Reaction.js
@@ -3,6 +3,16 @@ const {Schema, model} = require('mongoose');
 // Schema only
 const reactionSchema = new Schema(
     {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId,
+
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280,
+        },
         username: {
             type: String,
             required: true,
